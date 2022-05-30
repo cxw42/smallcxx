029fa6403a3fd74765c7b537033be06f483d9009
Chris White <cxwembedded@gmail.com>
2022-05-29 21:21:25 -0400

    t/*.sh: use $tpgmdir instead of $here
    This fixes a distcheck failure.

--- t/logging-t.sh
+++ t/logging-t.sh
@@ -12,26 +12,26 @@ main() {
     unset V
     unset LOG_LEVELS
 
-    "$here/log-debug-message-s" &> "$tmpfile"
+    "$tpgmdir/log-debug-message-s" &> "$tmpfile"
     does-not-contain 'avocado' "$tmpfile"
 
-    V=10 "$here/log-debug-message-s" &> "$tmpfile"
+    V=10 "$tpgmdir/log-debug-message-s" &> "$tmpfile"
     has-line-matching 'avocado' "$tmpfile"
 
     # Explicit log levels.  log-explicit-domain logs "avocado" @ DEBUG to +fruit
-    LOG_LEVELS='*:10' "$here"/log-explicit-domain-s &> "$tmpfile"
+    LOG_LEVELS='*:10' "$tpgmdir"/log-explicit-domain-s &> "$tmpfile"
     does-not-contain 'avocado' "$tmpfile"
 
-    LOG_LEVELS='*:0,+fruit:10' "$here"/log-explicit-domain-s &> "$tmpfile"
+    LOG_LEVELS='*:0,+fruit:10' "$tpgmdir"/log-explicit-domain-s &> "$tmpfile"
     has-line-matching 'avocado' "$tmpfile"
 
-    LOG_LEVELS='*:0,+fruit:5' "$here"/log-explicit-domain-s &> "$tmpfile"
+    LOG_LEVELS='*:0,+fruit:5' "$tpgmdir"/log-explicit-domain-s &> "$tmpfile"
     has-line-matching 'avocado' "$tmpfile"
 
-    LOG_LEVELS='+fruit:5,*:0' "$here"/log-explicit-domain-s &> "$tmpfile"
+    LOG_LEVELS='+fruit:5,*:0' "$tpgmdir"/log-explicit-domain-s &> "$tmpfile"
     has-line-matching 'avocado' "$tmpfile"
 
-    LOG_LEVELS='*:0,+fruit:4' "$here"/log-explicit-domain-s &> "$tmpfile"
+    LOG_LEVELS='*:0,+fruit:4' "$tpgmdir"/log-explicit-domain-s &> "$tmpfile"
     does-not-contain 'avocado' "$tmpfile"
 
     report-and-exit 0
