564692e28de4d0bd38dcdbed98eebb4cba6ac025
Chris White <cxwembedded@gmail.com>
2022-06-30 20:04:24 -0400

    Add tests of userdata

---
+++ t/globstari-userdata-t.cpp
@@ -0,0 +1,84 @@
+/// @file t/globstari-userdata-t.cpp
+/// @brief Test smallcxx::Entry::userdata
+/// @author Christopher White <cxwembedded@gmail.com>
+/// @copyright Copyright (c) 2021--2022 Christopher White
+
+#include <set>
+#include <utility>
+
+#include "smallcxx/globstari.hpp"
+#include "smallcxx/test.hpp"
+
+TEST_FILE
+
+using namespace smallcxx;
+using namespace std;
+using smallcxx::glob::Path;
+
+/// An IFileTree that produces a virtual filesystem.
+/// The filesystem includes exactly directory `/` and file `/file`.
+class TestFileTreeUserdata: public IFileTree
+{
+public:
+    std::vector<Entry>
+    readDir(const Path& dirPath) override
+    {
+        std::vector<Entry> retval;
+
+        if(dirPath == "/") {
+            Entry e{EntryType::File, "/file"};
+            e.userdata = 42;
+            retval.push_back(std::move(e));
+        }
+
+        return retval;
+    }
+
+    Bytes
+    readFile(const Path& path) override
+    {
+        return "";
+    }
+
+    Path
+    canonicalize(const Path& path) const
+    {
+        return path;
+    }
+}; // class TestFileTreeUserdata
+
+/// Save files traversed.
+class SaveEntries: public IProcessEntry
+{
+public:
+    std::vector<Entry> found;
+
+    IProcessEntry::Status
+    operator()(const Entry& entry) override
+    {
+        LOG_F(TRACE, "Found %s", entry.canonPath.c_str());
+        if(entry.ty == EntryType::File) {
+            found.push_back(entry);
+        }
+        return IProcessEntry::Status::Continue;
+    }
+
+}; // SaveEntries
+
+static void
+test_userdata()
+{
+    TestFileTreeUserdata fileTree;
+    SaveEntries processEntry;
+    reached();
+
+    globstari(fileTree, processEntry, "/", {"*"});
+    reached();
+
+    cmp_ok(processEntry.found.size(), ==, 1);
+    cmp_ok(processEntry.found[0].userdata, ==, 42);
+}
+
+TEST_MAIN {
+    TEST_CASE(test_userdata);
+}
